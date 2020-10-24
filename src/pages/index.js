24d7719af51b65d6d7e4d282addea76ce6e97c7e
chadwithuhc <chadwithuhc@gmail.com>
2020-10-24 12:34:35 -0600

    Content Card content and styling changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,30 @@
 import Layout from 'src/components/Layout'
 import MainNav from 'src/components/MainNav'
+import Link from 'next/link'
 
 const bits = [
-  { slug: 'definition-lists', title: 'Definition Lists', tags: ['semantic html', 'key value'] },
-  { slug: 'javascript-set-constructor', title: 'JavaScript Set Contructor', tags: ['javascript', 'es6', 'dedupe'] }
+  {
+    slug: 'definition-lists',
+    title: 'Key/Value pairs with Definition Lists',
+    tags: ['semantic html', 'key value'],
+    description: 'Display Key / Value pairs on the page using Definition List `<dl>` element',
+    headers: [
+      'Why a Definition List over X, Y, Z?',
+      'Alternative ways to build a Key/Value list'
+    ]
+  },
+  {
+    slug: 'javascript-set-constructor',
+    title: 'JavaScript Set Contructor',
+    tags: ['javascript', 'es6', 'dedupe'],
+    description: 'Explore use cases for the JavaScript Set Contructor as well as limitation and gotchas.',
+    headers: [
+      'What is the purpose of `Set`?',
+      'Is `Set` supported?',
+      'How can I deduplicate with a `Set`?',
+      '`Set` Gotchas'
+    ]
+  },
 ]
 
 export default function Home() {
@@ -14,9 +35,18 @@ export default function Home() {
         <section className="content-grid">
           {bits.map(bit => (
             <article key={bit.slug} className="content-card">
+              <img src="/code-sample-screenshot.png" className="content-card-image" />
               <h2 className="content-card-title">{bit.title}</h2>
               <p className="content-tags">{bit.tags.map(tag => <span key={tag} className="content-tags-tag">{tag}</span>)}</p>
-              <img src="/code-sample-screenshot.png" className="content-card-image" />
+              <p className="content-card-description">{bit.description}</p>
+              <ul className="content-card-deep-links">
+                {bit.headers.map(header => (
+                  <li key={header}>
+                    <Link href="#"><a>{header}</a></Link>
+                  </li>
+                ))}
+              </ul>
+              <Link href="#"><a>Continue Reading &gt;</a></Link>
             </article>
           ))}
         </section>
