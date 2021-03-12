7b393180084565680d3bd2a11bec4b0ff3040bc3
chadwithuhc <chadwithuhc@gmail.com>
2021-03-11 21:55:23 -0700

    Favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,13 @@ export default function Layout({ title, children = [] }) {
     <div className="main-layout">
       <Head>
         <title>{title} / ched.dev</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#daa520" />
+        <meta name="msapplication-TileColor" content="#daa520" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       {children}
     </div>
