58ff98a5b302ac149c2a2cd3ffbeb6e771036311
chadwithuhc <chadwithuhc@gmail.com>
2021-03-12 21:33:45 -0700

    CSS gap fix

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -6,10 +6,10 @@ export default function MainNav() {
       <div className="main-header-links">
         <Link href="/" title="home"><a className="main-name">ched.dev</a></Link>
         <nav className="main-nav">
-          {/* <Link href="/code/bits"><a>Code in Bits</a></Link> */}
+          <Link href="/code/bits"><a>Code in Bits</a></Link>
           <Link href="/code/projects"><a>Code Projects</a></Link>
-          {/* <Link href="/code/challenges"><a>Code Challenges</a></Link> */}
-          {/* <Link href="/code/live-streams"><a>Code Live Streams</a></Link> */}
+          <Link href="/code/challenges"><a>Code Challenges</a></Link>
+          <Link href="/code/live-streams"><a>Code Live Streams</a></Link>
           
           {/* <Link href="/code/interview-prep"><a>Interview Prep</a></Link> */}
         </nav>
